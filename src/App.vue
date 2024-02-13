ca817067d2d7c8d5b9c4ade1c2c95f7b8cff60b7
robot <cgajula152@gmail.com>
2024-02-13 17:27:08 +0530

    implemented vue-draggable

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +24,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  margin-right: 220px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,60 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-5">
+          <h4 class="mb-3">Draggable 1</h4>
+          <!-- pull option to clone means that dragging an element out of this list will clone the element and not move it permanently out of the list.
+             And setting put to false means we can’t drag new elements into this group. -->
+          <draggable
+            class="draggable-list"
+            :list="list1"
+            :group="{
+              name: 'mygroup',
+              pull: 'clone',
+              put: false,
+            }"
+          >
+            <div class="list-item" v-for="element in list1" :key="element">
+              {{ element }}
+            </div>
+          </draggable>
+        </div>
+
+        <div class="col-5">
+          <h4 class="mb-3">Draggable 2</h4>
+          <draggable
+            class="draggable-list"
+            :list="list2"
+            :group="{ name: 'mygroup' }"
+          >
+            <div class="list-item" v-for="element in list2" :key="element">
+              {{ element }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
+import draggable from "vuedraggable";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      list1: ["ABC", "DEF", 3, 4],
+      list2: ["GHI", "JKL", 7, 8],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -48,11 +66,33 @@ ul {
   list-style-type: none;
   padding: 0;
 }
+.hello {
+  background-color: #42b983;
+  width: auto;
+  height: auto;
+}
 li {
   display: inline-block;
   margin: 0 10px;
 }
 a {
   color: #42b983;
+}
+.draggable-list {
+  background: linear-gradient(180deg, #32294d, #6b5e9a, #ab9bd8);
+  color: #fff;
+  border: 1px solid;
+  border-radius: 10px;
+  height: 30vh;
+}
+.list-item {
+  margin: 10px;
+  padding: 40px;
+  cursor: pointer;
+  font-size: 18px;
+  border-radius: 5px;
+  border: 2px solid black;
+  background: linear-gradient(#32294d, #6b5e9a, #ab9bd8);
+  display: inline-block;
 }
 </style>
