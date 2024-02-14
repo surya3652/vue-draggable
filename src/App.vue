fbda8a5f73fe582b5d81af8f5d2e4e3dc10b6346
robot <cgajula152@gmail.com>
2024-02-14 11:25:16 +0530

    Implemented different cases in working with vue draggable

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Vue Draggable</h1>
+    <hr />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
